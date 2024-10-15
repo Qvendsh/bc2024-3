11aa015072abfe034c67baec786adadce8b0e8fa
Qvendsh bc2024-3 <qvendsh@gmail.com>
2024-10-15 21:48:28 +0300

    removed some mistakes

--- index.js
+++ index.js
@@ -1,32 +1,43 @@
-const { program } = require('commander');
 const fs = require('fs');
+const { program } = require('commander');
 
-program
-  .requiredOption('-i, --input <path>', 'input file path')
-  .option('-o, --output <path>', 'output file path')
-  .option('-d, --display', 'display result in console');
-
-program.parse(process.argv);
+program    
+    .requiredOption('-i, --input <path>', 'input JSON file')
+    .option('-o, --output <path>', 'output file')   
+    .option('-d, --display', 'display result in console')
+    .parse(process.argv);
 
 const options = program.opts();
 
-
-if (!fs.existsSync(options.input)) {
-  console.error('Cannot find input file');
-  process.exit(1);
+if (!fs.existsSync(options.input)) {    
+    console.error("Error: Cannot find input file");
+    process.exit(1); 
 }
 
-const data = fs.readFileSync(options.input, 'utf8');
+let data;
+try {
+    data = fs.readFileSync(options.input, 'utf-8');
+} catch (error) {
+    console.error('Error: Unable to read input file');
+    process.exit(1);
+}
 
+let parsedData;
+try {
+    parsedData = JSON.parse(data);
+} catch (error) {
+    console.error('Error: Input file is not valid JSON');
+    process.exit(1);
+}
+
+const resultLines = parsedData.map(entry => {
+    return `${entry.exchangedate}: ${entry.rate}`;
+});
 
 if (options.display) {
-  console.log(data);
+    resultLines.forEach(line => console.log(line));
 }
 
 if (options.output) {
-  fs.writeFileSync(options.output, data);
-}
-
-
-if (!options.display && !options.output) {
+    fs.writeFileSync(options.output, resultLines.join('\n'), 'utf-8');
 }
